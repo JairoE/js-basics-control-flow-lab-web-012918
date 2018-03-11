ab8e51d526fac75e107953e1a0917b4d352c5f32
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-03-11 21:36:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function scuberGreetingForFeet(num) {
   if num <= 400 {
     return 'This one is on me!';
   } else if (num > 2000) {
-    return "I will gladly take your thirty bucks.";
+    return 'I will gladly take your thirty bucks.';
   } else if (num > 2500) {
-    return "No can do.";
+    return 'No can do.';
   }
 }
