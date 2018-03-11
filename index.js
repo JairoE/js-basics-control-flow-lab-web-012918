c8e34226b596e48797170725d953dbf29f45cee1
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-03-11 21:44:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function scuberGreetingForFeet (num) {
 
 
 function ternaryCheckCity(city) {
-  city === "NYC" ? "Ok, sounds good." : "No go."
+  (city === "NYC") ? "Ok, sounds good." : "No go."
 }
