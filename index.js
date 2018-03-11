69d9a4c37dc47756e88fa56473f484a376eebb90
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-03-11 21:41:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Write your code in this file!
-function scuberGreetingForFeet (x) {
-  if x <= 400 {
+function scuberGreetingForFeet (num) {
+  if (num <= 400) {
     console.log('This one is on me!');
-  } else if (x > 2000) {
+  } else if (num > 2000) {
     return 'I will gladly take your thirty bucks.';
-  } else if (x > 2500) {
+  } else if (num > 2500) {
     return 'No can do.';
   }
 }
